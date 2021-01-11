9ea312c144edfa6e812d4a387f130766ecaa263f
ishitparikh <ishitparikh_mca@yahoo.in>
2021-01-11 17:51:04 +0530

    Using objects

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import React, { Component } from "react";
 import "./styles.css";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Ishit",
+      user: {
+        firstName: "Harper",
+        lastName: "Perez"
+      }
+    };
+    this.name = "Ishit";
+  }
+
+  formatName(user) {
+    if (user) {
+      return " " + user.firstName + " " + user.lastName;
+    }
+    return "Stranger";
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <h1>Hello {this.name}</h1>
+        <h1>Message from {this.formatName(this.state.user)}</h1>
       </div>
     );
   }
